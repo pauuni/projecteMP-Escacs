57bd34740a002fd9fda6a1f71d6f7f8529e5bb9c
pauuni <57673106+pauuni@users.noreply.github.com>
2022-04-18 13:09:16 +0200

    Create main.cpp
    Versio correu Albert

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+#include "ChessBoard.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	VecOfPositions vector;
+	ChessBoard vector1;
+	ChessPosition v1;
+	v1.setPosX(4);
+	v1.setPosY(4);
+	string v=vector1.ToString();
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	vector = vector1.GetValidMoves(v1);
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	v = vector1.ToString();
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	for (int i = 0; i < vector.size(); i++)
+	{
+		cout << "(" << vector[i].getPosX() << "," << vector[i].getPosY() << ")";
+	}
+	return 0;
+}
