462974204d750d0094a18ea8fb05bec5891a580a
pauuni <57673106+pauuni@users.noreply.github.com>
2022-04-18 13:10:07 +0200

    Create Piece.h

---
+++ Piece.h
@@ -0,0 +1,43 @@
+
+#ifndef Piece_h
+#define Piece_h
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+typedef enum {
+    CPT_Rei = 'R',
+    CPT_Dama = 'D',
+    CPT_Torre = 'T',
+    CPT_Alfil = 'A',
+    CPT_Cavall = 'C',
+    CPT_Peó = 'P',
+    CPT_EMPTY = '_'
+} ChessPieceType;
+
+typedef enum {
+    CPC_Blanc = 'B',
+    CPC_Negra = 'N',
+    CPC_NONE = '_'
+} ChessPieceColor;
+
+
+
+class Piece {
+
+public:
+    Piece() : m_type(CPT_EMPTY), m_color(CPC_NONE) {}
+    ChessPieceType getChessPieceType() { return m_type; }
+    ChessPieceColor getChessPieceColor() { return m_color; }
+    void setChessPieceType(ChessPieceType type) { m_type = type; }
+    void setChessPieceColor(ChessPieceColor color) { m_color = color; }
+
+private:
+    ChessPieceType  m_type;
+    ChessPieceColor m_color;
+ 
+};
+
+
+#endif
