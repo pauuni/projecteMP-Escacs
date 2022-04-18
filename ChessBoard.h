2e768de4da8219d5db7a2af3f335e0c22470722b
pauuni <57673106+pauuni@users.noreply.github.com>
2022-04-18 13:09:38 +0200

    Create ChessBoard.h

---
+++ ChessBoard.h
@@ -0,0 +1,35 @@
+
+#ifndef ChessBoard_h
+#define ChessBoard_h
+
+#include <stdio.h>
+#include <vector>
+#include "Piece.h"
+#include "ChessPosition.h"
+#include <iostream>
+using namespace std;
+
+int const NUM_COLS = 8;
+int const NUM_ROWS = 8;
+typedef vector<ChessPosition> VecOfPositions;       /*//??*/
+
+class ChessBoard {
+
+public:
+    ChessBoard(); 
+    
+    void LoadBoardFromFile(const string& path);  //esto con ifstream para leer de documento. 
+    bool MovePiece(const ChessPosition& posFrom, const ChessPosition& posTo);
+    void setPiece(int col, int row, ChessPieceType  tipo, ChessPieceColor color);
+
+    VecOfPositions GetValidMoves (const ChessPosition& pos) ;
+    string ToString () ;
+    Piece getPiece(int col, int row) { return m_board[col][row]; }
+
+private:
+    Piece m_board[NUM_COLS][NUM_ROWS];
+    
+};
+
+void afejirPosicio(VecOfPositions& vec, int x, int y);
+#endif
