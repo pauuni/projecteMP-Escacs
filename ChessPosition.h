af9fc4b30a200539f2df0a234d90d4edba3ecae6
pauuni <57673106+pauuni@users.noreply.github.com>
2022-05-06 13:22:15 +0200

    sobrecarrega ==

--- ChessPosition.h
+++ ChessPosition.h
@@ -1,3 +1,6 @@
+//
+//  ChessPosition.hpp
+//
 
 #ifndef ChessPosition_h
 #define ChessPosition_h
@@ -9,6 +12,9 @@ using namespace std;
 class ChessPosition {
 
 public://metodo: posicio  esta en el tablero , sumar de posicions
+    //ChessPosition(string pos) : m_posX(correctXCoordenate(pos[1] - 48)), m_posY(stringToYCoordenate(charToString(pos[0]))) {}; //es la versio optima, no funciona
+    ChessPosition(string pos) : m_posX((pos[1] - 48) - 1), m_posY((pos[0]) - 'a') {};
+    ChessPosition() : m_posX(0), m_posY(0) {}; //setejar els valors a 0 ?
     void setPosX(int x) { m_posX = x; }
     void setPosY(int y) { m_posY = y; }
     int getPosX() const { return m_posX; }
@@ -16,9 +22,8 @@ public://metodo: posicio  esta en el tablero , sumar de posicions
 private:
     int m_posX; //Column
     int m_posY; //Row
-
-
 };
 
+bool operator==(const ChessPosition& p1, const ChessPosition& p2);
 
 #endif
